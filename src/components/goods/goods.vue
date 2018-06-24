e839a8e62044a462d3f0c38432a47dbdf95da20b
HuangChenITer <crystalhc225@163.com>
2018-06-24 21:26:21 +0800

    test

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="goods">
+      我是 goods
+    </div>
+</template>
+
+<script type="test/ecmascript-6">
+  export default{};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
